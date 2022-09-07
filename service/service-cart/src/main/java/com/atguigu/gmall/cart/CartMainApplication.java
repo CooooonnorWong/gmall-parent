c695a5f0a8f8da7a5442bee47a26b1a3dc23d3a3
Connor <782255966@outlook.com>
2022-09-08 03:34:42 +0800

    完成:   - service-cart(服务):     - 主启动类

---
+++ service/service-cart/src/main/java/com/atguigu/gmall/cart/CartMainApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author Connor
+ * @date 2022/9/8
+ */
+@SpringCloudApplication
+public class CartMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartMainApplication.class, args);
+    }
+}
